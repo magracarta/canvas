1cfbb1124c2029053e585493235ae36f39a6bd9b
magracarta <rmfoal1996@gmail.com>
2022-08-02 12:53:42 +0900

    공부

---
+++ box애니메이션/panel.js
@@ -0,0 +1,12 @@
+class Panel {
+    constructor(){
+        this.draw();
+    }
+
+    draw(){
+        context.fillStyle = 'rgba(255,0,0,0.8)';
+        context.fillRect(oX-150, oY-150, 300, 300);
+        context.fillStyle = '#fff';
+        context.fillText();
+    }
+}
