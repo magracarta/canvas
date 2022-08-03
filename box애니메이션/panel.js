df05644cd68d57b6955771eb569210518d2d43ca
magracarta <rmfoal1996@gmail.com>
2022-08-03 23:27:56 +0900

    짧게 공부

--- box애니메이션/panel.js
+++ box애니메이션/panel.js
@@ -4,9 +4,13 @@ class Panel {
     }
 
     draw(){
+        console.log(oX, oY);
         context.fillStyle = 'rgba(255,0,0,0.8)';
         context.fillRect(oX-150, oY-150, 300, 300);
         context.fillStyle = '#fff';
-        context.fillText();
+        // context.fillText();
+        if(selectedBox){
+            context.fillText(selectedBox.index, oX,oY);
+        }
     }
 }
